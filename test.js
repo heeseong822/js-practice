5ad1a09b4ce5e66370c1e81d5258385d46e27520
정희성 <ywindj301877@naver.com>
2026-09-17 18:56:34 +0900

    첫 커밋: 배열 연습 코드

---
+++ test.js
@@ -0,0 +1,13 @@
+const users = [
+  { name: "김철수", age: 28, active: true },
+  { name: "이영희", age: 34, active: false },
+];
+
+const names = users.filter(u => u.active).map(u => u.name);
+console.log(names);
+
+
+
+
+
+
