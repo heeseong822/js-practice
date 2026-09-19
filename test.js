bcd0ab82d0b5591deb3363f33174f9d802360452
정희성 <ywindj301877@naver.com>
2026-09-19 22:20:15 +0900

    테스트: 일부러 코드 망가뜨림

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const users = [
   { name: "김철수", age: 28, active: true },
-  { name: "이영희", age: 34, active: false },
+  { name: "이영희", age: 34, active: false 
+,
 ];
 
 const names = users.filter(u => u.active).map(u => u.name);
